4839a3262f827ec72a973d637dd987eed1c8c171
Quang Hai <quanghai.ng1512@gmail.com>
2018-04-24 00:11:27 +0700

    Update project config

--- ConvenientShop.API/Startup.cs
+++ ConvenientShop.API/Startup.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using ConvenientShop.API.Helpers;
+using ConvenientShop.API.Models;
+using ConvenientShop.API.Services;
+using ConvenientShop.API.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,9 +19,15 @@ namespace ConvenientShop.API
 {
     public class Startup
     {
+        private readonly MapperConfiguration _mapperConfiguration;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            _mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfileConfiguration());
+            });
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +35,14 @@ namespace ConvenientShop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(obj => obj.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+
+            services.Configure<StoreConfig>(Configuration.GetSection("ApiConfig"));
+            
+            services.AddScoped<IConvenientStoreRepository, ConvenientStoreRepository>();
+            services.AddScoped<IStaffRepository, StaffRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +54,23 @@ namespace ConvenientShop.API
             }
 
             app.UseMvc();
+
+            //AutoMapper.Mapper.Initialize(config =>
+            //{
+            //    config.CreateMap<Entities.Staff, Models.StaffDto>()
+            //        .ForMember(dest => dest.StaffName,
+            //            opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+            //        .ForMember(dest => dest.Age,
+            //            opt => opt.MapFrom(src => Helpers.Helpers.DateOfBirthToAge(src.DateOfBirth)))
+            //        .ForMember(dept => dept.Gender,
+            //            opt => opt.MapFrom(src => src.Gender == true ? "Male" : "Female"));
+            //    config.CreateMap<Entities.Product, Models.ProductDto>()
+            //        .ForMember(dest => dest.SupplierName,
+            //            opt => opt.MapFrom(src => src.Supplier.SupplierName))
+            //        .ForMember(dest => dest.Category,
+            //            opt => opt.MapFrom(src => src.Category.Name));
+            //});
+
         }
     }
 }
