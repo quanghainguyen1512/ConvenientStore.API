6443f82c30494372d6b5a2fa44efb0250070b188
Quang Hai <quanghai.ng1512@gmail.com>
2018-04-24 00:08:12 +0700

    Update Helpers

---
+++ ConvenientShop.API/Helpers/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvenientShop.API.Helpers
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        public AutoMapperProfileConfiguration()
+        {
+            CreateMap<Entities.Staff, Models.StaffDto>()
+                .ForMember(dest => dest.StaffName,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => Helpers.DateOfBirthToAge(src.DateOfBirth)))
+                .ForMember(dept => dept.Gender,
+                    opt => opt.MapFrom(src => src.Gender == true ? "Male" : "Female"));
+            CreateMap<Entities.Product, Models.ProductDto>()
+                .ForMember(dest => dest.SupplierName,
+                    opt => opt.MapFrom(src => src.Supplier.SupplierName))
+                .ForMember(dest => dest.Category,
+                    opt => opt.MapFrom(src => src.Category.Name));
+        }
+    }
+}
---
+++ ConvenientShop.API/Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvenientShop.API.Helpers
+{
+    public class Helpers
+    {
+        public static int DateOfBirthToAge(DateTime dob)
+        {
+            var ts = DateTime.Now.Subtract(dob);
+            var tempDate = DateTime.MinValue.Add(ts);
+
+            return tempDate.Year - 1;
+        }
+    }
+}
