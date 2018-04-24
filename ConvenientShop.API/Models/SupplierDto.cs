e6a57a3812b307a2a6c19daed640019943b8ff0a
QuangHai_Ubuntu <quanghai.ng@outlook.com.vn>
2018-04-25 00:29:16 +0700

    Update Models: Edit Supplier, Add SupplierWithoutProduct

--- ConvenientShop.API/Models/SupplierDto.cs
+++ ConvenientShop.API/Models/SupplierDto.cs
@@ -11,5 +11,7 @@ namespace ConvenientShop.API.Models
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public ICollection<ProductDto> Products { get; set; } = new List<ProductDto>();
+        public int NumberOfProducts => Products.Count;
     }
 }
---
+++ ConvenientShop.API/Models/SupplierWithoutProductsDto.cs
@@ -0,0 +1,10 @@
+namespace ConvenientShop.API.Models
+{
+    public class SupplierWithoutProductsDto
+    {
+        public string SupplierName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
