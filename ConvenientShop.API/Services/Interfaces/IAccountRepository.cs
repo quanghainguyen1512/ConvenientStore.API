adfb2aecf02d55eb14fbfda8801f27d8e9feec7c
QuangHai_Ubuntu <quanghai.ng@outlook.com.vn>
2018-04-24 11:15:35 +0700

    Edit AccountRepo from ubuntu

--- ConvenientShop.API/Services/Interfaces/IAccountRepository.cs
+++ ConvenientShop.API/Services/Interfaces/IAccountRepository.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConvenientShop.API.Models;
 
 namespace ConvenientShop.API.Services.Interfaces
 {
     public interface IAccountRepository
     {
         bool AuthorizeUser(int userId, Permission perm);
-        (bool isSuccessful, int accountId) LogIn(string username, string password);
         bool IsUsernameExists(string username);
         void CreateAccount(string username, string password, int roleId);
+        (bool, AccountDto) LogIn(string username, string password);
     }
 }
