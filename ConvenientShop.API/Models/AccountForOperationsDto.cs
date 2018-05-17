28357a98de3c090ec10e319579698f3d6deabe3d
Quang Hai <quanghai.ng1512@gmail.com>
2018-05-17 08:54:09 +0700

    Operations with account (final)

--- ConvenientShop.API/Models/AccountForOperationsDto.cs
+++ ConvenientShop.API/Models/AccountForOperationsDto.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ConvenientShop.API.Validators;
 
 namespace ConvenientShop.API.Models
 {
@@ -10,5 +12,15 @@ namespace ConvenientShop.API.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public int RoleId { get; set; }
+        public(bool isValid, IEnumerable<ValidationResult> errors) Validate()
+        {
+            var validator = new AccountValidator();
+            var result = validator.Validate(this);
+
+            if (result.IsValid)
+                return (true, null);
+
+            return (false, result.Errors.Select(item => new ValidationResult(item.ErrorMessage, new [] { item.PropertyName })));
+        }
     }
 }
