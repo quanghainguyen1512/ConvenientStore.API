49e676c6716de81b82e800cc0a104062fe06ee55
Quang Hai <quanghai.ng1512@gmail.com>
2018-05-25 10:08:09 +0700

    Temp

---
+++ ConvenientShop.API/Controllers/ReportController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConvenientShop.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/delivery")]
+    public class ReportController : Controller
+    {
+        public ReportController()
+        {
+            
+        }
+    }
+}
