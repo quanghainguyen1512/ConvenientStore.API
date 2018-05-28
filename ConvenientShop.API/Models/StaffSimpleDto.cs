6efa4638818877aeaef90e2be544a31e2ba28aac
Quang Hai <quanghai.ng1512@gmail.com>
2018-05-28 21:25:19 +0700

    modify output of staff

--- ConvenientShop.API/Models/StaffSimpleDto.cs
+++ ConvenientShop.API/Models/StaffSimpleDto.cs
@@ -2,6 +2,7 @@ namespace ConvenientShop.API.Models
 {
     public class StaffSimpleDto
     {
+        public int StaffId { get; set; }
         public string FullName { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
