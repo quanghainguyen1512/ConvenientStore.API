fdf3b1bb45913e636f10a1d863878aee40f6f904
Quang Hai <quanghai.ng1512@gmail.com>
2018-05-29 22:50:16 +0700

    authorization for view product details needed

--- ConvenientShop.API/Controllers/ProductDetailController.cs
+++ ConvenientShop.API/Controllers/ProductDetailController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using AutoMapper;
 using ConvenientShop.API.Helpers;
 using ConvenientShop.API.Models;
+using ConvenientShop.API.Services;
 using ConvenientShop.API.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,14 +15,19 @@ namespace ConvenientShop.API.Controllers
     public class ProductDetailController : Controller
     {
         private readonly IProductRepository _prepo;
-        public ProductDetailController(IProductRepository prepo)
+        private readonly IAccountRepository _arepo;
+
+        public ProductDetailController(IProductRepository prepo, IAccountRepository arepo)
         {
             this._prepo = prepo;
+            this._arepo = arepo;
         }
 
         [HttpGet]
-        public IActionResult GetProductDetails(ProductDetailsResourceParameters parameters)
+        public IActionResult GetProductDetails(ProductDetailsResourceParameters parameters, int accountId = -1)
         {
+            if (!_arepo.AuthorizeUser(accountId, Permission.ViewProductDetail))
+                return Unauthorized();
             var (pds, count) = _prepo.GetAllProductDetails(parameters);
             var result = Mapper.Map<PagedList<ProductDetailSimpleDto>>(pds);
             result.AddMoreDetail(count, parameters.PageNumber, parameters.PageSize);
